9f9e77ab532869bafbfbf9f3c44f3f5362fe122f
David Rodriguez <devdav0795@gmail.com>
2019-12-01 22:14:53 -0600

    modal

--- js/script.js
+++ js/script.js
@@ -16,5 +16,28 @@ function DateTrip() {
     }
 }
 
+const modal = document.getElementById('modal');
+const overlay = document.getElementById('overlay');
+const buttonImprimir= document.getElementById("imprimirBoleto")
+const buttonhide = document.getElementById("hide-modal")
+
+buttonhide.addEventListener('click', () => {
+    hideModal()
+  })
+
+function hideModal() {
+  overlay.classList.remove('active');
+  modal.style.animation = 'modalOut .8s forwards';
+}
+
+buttonImprimir.addEventListener('click', () => {
+      showModal()
+    })
+
+function showModal() {
+    overlay.classList.add('active');
+    modal.style.animation = 'modalIn .8s forwards';
+    }
+
 
 
