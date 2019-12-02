5dcf0ae32e60805cc07c4f6b024fc29adf11c075
David Rodriguez <davidre0795@gmail.com>
2019-12-02 09:17:39 -0600

    modal

--- js/script.js
+++ js/script.js
@@ -16,5 +16,31 @@ function DateTrip() {
     }
 }
 
+function addEventClick($element) {
+    $element.addEventListener('click', () => {
+      // alert('click')
+      showModal($element)
+    })
+  }
+
+function showModal($element) {
+    $overlay.classList.add('active');
+    $modal.style.animation = 'modalIn .8s forwards';
+    const id = $element.dataset.id;
+    const category = $element.dataset.category;
+    const data = findMovie(id, category);
+
+    $modalTitle.textContent = data.title;
+    $modalImage.setAttribute('src', data.medium_cover_image);
+    $modalDescription.textContent = data.description_full
+  }
+
+  $hideModal.addEventListener('click', hideModal);
+  function hideModal() {
+    $overlay.classList.remove('active');
+    $modal.style.animation = 'modalOut .8s forwards';
+
+  }
+
 
 
